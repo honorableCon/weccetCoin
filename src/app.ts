1b0f9efe1f3c52169d2f19f2c865a51ba13741d0
Honorable con <honorablecon@gmail.com>
2022-07-25 22:46:46 +0000

    Add api routes

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,29 @@ import Blockchain from './core/blockchain'
 import Block from './core/block'
 import Transaction from './core/transaction'
 
+const express = require('express')
+const app = express();
+const port = process.argv[2] || 8020
 const blockchain = new Blockchain();
-const transaction = new Transaction("bibou", "penda", 123);
-
 const block = new Block();
-block.addTransaction(transaction);
-blockchain.addBlock(block);
-console.log(blockchain);
+
+app.use(express.json())
+
+
+app.get('/blockchain', (req:any, res:any) => {
+    blockchain.addBlock(block);
+    return res.json(blockchain);
+})
+
+
+app.post('/transactions', async (req:any, res:any) => {
+    const { from, to, amount } = req.body;
+    const transaction = new Transaction(from, to, amount);
+    block.addTransaction(transaction);
+    return res.json(block);
+})
+
+
+app.listen(port, ()=>{
+    console.log('listening on port', port);
+})
